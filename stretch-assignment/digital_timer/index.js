3ef12aaecfe20f8fdb869dac629edd9aacae9e30
Max Webb <webbwrites@gmail.com>
2019-07-10 20:56:16 -0600

    Displays 0 in all 4 places. Still does not work

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,15 +2,28 @@ const getLilHundreds =   document.getElementById('msHundreds');
 
 getLilHundreds.textContent = 0;
 
-const lilHundreds = setInterval(incLilHundreds, 10);
+// const lilHundreds = setInterval(incLilHundreds, 10);
 
-const incLilHundreds = (num) => {
-  if (num>=0) {
-    num++
-  } else if (num==9) {
-    return
-  }
-};
+// const incLilHundreds = (num) => {
+//   if (num<=9) {
+//     num++
+//   } else if (num==9) {
+//     return
+//   }
+// };
+
+const getLilTens =   document.getElementById('msTens');
+
+getLilTens.textContent = 0;
+
+const getBigOnes =   document.getElementById('secondOnes');
+
+getBigOnes.textContent = 0;
+
+const getBigTens =   document.getElementById('secondTens');
+
+getBigTens.textContent = 0;
+
 
 // const lilTens = setInterval(getLilTens, 100);
 //
@@ -20,6 +33,6 @@ const incLilHundreds = (num) => {
 //
 // const getBigOnes = () => {};
 //
-// const bigHundreds = setInterval(getBigTens, 10000);
+// const bigTens = setInterval(getBigTens, 10000);
 //
 // const getBigTens = () => {};
