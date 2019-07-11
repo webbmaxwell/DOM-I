f82e547b75533b28885d150699d73b9e8f9cca50
Max Webb <webbwrites@gmail.com>
2019-07-10 20:22:46 -0600

    Added script to HTML and started JS

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,15 @@
+const lilHundreds = setInterval(getLilHundreds, 10);
+
+const getLilHundreds = () => {};
+
+const lilTens = setInterval(getLilTens, 100);
+
+const getLilTens = () => {};
+
+const bigOnes = setInterval(getBigOnes, 1000);
+
+const getBigOnes = () => {};
+
+const bigHundreds = setInterval(getBigHundreds, 10000);
+
+const getBigHundreds = () => {};
